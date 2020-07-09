1598ca8c717fac7747492ec4a7593cf630801e77
hyomin <8891ee1234@gmail.com>
2020-07-08 18:32:49 -0700

    Initial commit with sum modules

---
+++ sum.cpp
@@ -0,0 +1,6 @@
+#include "sum.h"
+#include <stdio.h>
+
+int main(){
+    printf("result : %d\n", sum(10));
+}
---
+++ sum.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int sum(int n){
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+
+    return sum;
+}
